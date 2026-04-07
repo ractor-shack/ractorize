53c20d1a83b93c8ef9cfe7f3814dd9bc31cd6429
Miles Georgi <azimux@gmail.com>
2026-04-06 20:11:15 -0700

    Come up with a proxy promise concept to see if we can maintain interfaces

---
+++ src/ractorize/proxy_promise.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+class ProxyPromise
+  attr_accessor :__target__, :__resolved__, :__value__
+
+  def initialize(__target__)
+    self.__target__ = __target__
+  end
+
+  def method_missing(method_name, *)
+    unless __resolved__
+      self.__value__ = __target__.receive
+      self.__resolved__ = true
+    end
+
+    __value__.send(method_name, *)
+  end
+end
