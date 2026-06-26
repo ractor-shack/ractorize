31a596e82cd2697abc71317dc878f66997a2052c
Miles Georgi <azimux@gmail.com>
2026-06-25 19:14:24 -0700

    Move TrackingRactor to its own file

--- src/ractorize/garbage_collection.rb
+++ src/ractorize/garbage_collection.rb
@@ -1,7 +1,5 @@
 module Ractorize
   module GarbageCollection
-    class TrackingRactor < BaseRactor; end
-
     class << self
       def track_ractorized_object(ractorized_object)
         # We have to define the finalizer here, not in the tracker, because it's not frozen yet
@@ -52,30 +50,5 @@ module Ractorize
         end
       end
     end
-
-    TRACKING_RACTOR = TrackingRactor.new do
-      tracker = Tracker.new
-
-      loop do
-        # SimpleCov branch coverage doesn't like that we aren't testing not matching anything
-        # but this does result in an error unlike case/when so no point in checking that.
-        # :nocov:
-        case receive
-          # :nocov:
-        in :track_ractorized_object, ractorized_object
-          tracker.track_ractorized_object(ractorized_object)
-        in :cleanup_after_ractorized_object, ractorized_object_id
-          tracker.cleanup_after_ractorized_object(ractorized_object_id)
-        in :track_thunk, thunk_id, thunk_ractor
-          tracker.track_thunk(thunk_id, thunk_ractor)
-        in :cleanup_after_thunk, thunk_id
-          tracker.cleanup_after_thunk(thunk_id)
-        end
-      rescue TrackingRactor::ClosedError
-        # do nothing
-      end
-    end
-
-    private_constant :TRACKING_RACTOR
   end
 end
---
+++ src/ractorize/garbage_collection/tracking_ractor.rb
@@ -0,0 +1,38 @@
+require_relative "../base_ractor"
+require_relative "tracker"
+
+module Ractorize
+  module GarbageCollection
+    class TrackingRactor < BaseRactor
+      class << self
+        def new
+          super(name: "GarbageCollection::TrackingRactor") do
+            tracker = Tracker.new
+
+            loop do
+              # SimpleCov branch coverage doesn't like that we aren't testing not matching anything
+              # but this does result in an error unlike case/when so no point in checking that.
+              # :nocov:
+              case receive
+                # :nocov:
+              in :track_ractorized_object, ractorized_object
+                tracker.track_ractorized_object(ractorized_object)
+              in :cleanup_after_ractorized_object, ractorized_object_id
+                tracker.cleanup_after_ractorized_object(ractorized_object_id)
+              in :track_thunk, thunk_id, thunk_ractor
+                tracker.track_thunk(thunk_id, thunk_ractor)
+              in :cleanup_after_thunk, thunk_id
+                tracker.cleanup_after_thunk(thunk_id)
+              end
+            rescue TrackingRactor::ClosedError
+              # do nothing
+            end
+          end
+        end
+      end
+    end
+
+    TRACKING_RACTOR = TrackingRactor.new
+    private_constant :TRACKING_RACTOR
+  end
+end
