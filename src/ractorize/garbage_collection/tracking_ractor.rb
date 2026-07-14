2eb88c7f8af4fce8fbacb98f62a8676c962c7b3e
Miles Georgi <azimux@gmail.com>
2026-07-13 23:41:09 -0700

    Collapse TrackingRactor case statement into one Object#send call

--- src/ractorize/garbage_collection/tracking_ractor.rb
+++ src/ractorize/garbage_collection/tracking_ractor.rb
@@ -10,22 +10,7 @@ module Ractorize
             tracker = Tracker.new
 
             loop do
-              # SimpleCov branch coverage doesn't like that we aren't testing not matching anything
-              # but this does result in an error unlike case/when so no point in checking that.
-              # :nocov:
-              case receive
-                # :nocov:
-              in :track_ractorized_object, ractorized_object
-                tracker.track_ractorized_object(ractorized_object)
-              in :cleanup_after_ractorized_object, ractorized_object_id
-                tracker.cleanup_after_ractorized_object(ractorized_object_id)
-              in :track_thunk, thunk_id, thunk_ractor
-                tracker.track_thunk(thunk_id, thunk_ractor)
-              in :thunk_cloned, old_thunk_id, new_thunk_id, thunk_ractor
-                tracker.thunk_cloned(old_thunk_id, new_thunk_id, thunk_ractor)
-              in :cleanup_after_thunk, thunk_id
-                tracker.cleanup_after_thunk(thunk_id)
-              end
+              tracker.send(*receive)
             rescue TrackingRactor::ClosedError
               # do nothing
             end
